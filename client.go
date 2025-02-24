e280833ac617ba2e49e0a4cd65514336494fb5bd
Hong Yao <yaohonget@gmail.com>
2025-02-24 14:16:59 +0100

    Add client basic

---
+++ client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial err : ", err)
+		return nil
+	}
+	client.conn = conn
+	return client
+}
+
+func main() {
+	client := NewClient("127.0.0.1", 8888)
+	if client == nil {
+		fmt.Println(">>>>>>> Connection failed <<<<<<<<<")
+		return
+	}
+	fmt.Println(">>>>>>> Connection successful <<<<<<<<<")
+
+	select {}
+}
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Server starting...")
-	server := NewServer("", 8888)
+	server := NewServer("127.0.0.1", 8888)
 	server.Start()
 	fmt.Println("Server started...")
 }
